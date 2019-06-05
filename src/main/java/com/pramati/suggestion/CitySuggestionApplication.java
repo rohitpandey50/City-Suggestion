4ab27a71c21f200b7bdf3cf8361e4ce7517b9951
Rohit Kumar <rohitkumar@Rohits-MacBook-Pro.local>
2019-06-05 13:39:31 +0530

    performance improved

--- src/main/java/com/pramati/suggestion/CitySuggestionApplication.java
+++ src/main/java/com/pramati/suggestion/CitySuggestionApplication.java
@@ -1,8 +1,13 @@
 package com.pramati.suggestion;
 
+import com.opencsv.CSVReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -10,9 +15,39 @@ import java.util.TreeSet;
 
 @SpringBootApplication
 public class CitySuggestionApplication {
+	static Logger logger = LoggerFactory.getLogger(CitySuggestionApplication.class);
 	public static List<String> cityList=new ArrayList<>();
 	public static void main(String[] args) {
 		SpringApplication.run(CitySuggestionApplication.class, args);
+		redRecordFromCSV();
+	}
+
+	/**Open CSV used to read csv file row wise and match prefix if found then add in separate set.
+	 * csv file also contain duplicate city name and case insensitive name.It will return unique suggestion name for city.
+	 * Used TreeSet to contain unique city name and ignore case insensitive .
+	 */
+	public static void redRecordFromCSV()
+	{
+		try {
+			logger.info("caching city List.....");
+			ClassPathResource cpr = new ClassPathResource("static/city_data.csv");
+			CSVReader csvReader = new CSVReader(new InputStreamReader(cpr.getInputStream()));
+			String[] nextRecord;
+			Set<String> cityList=new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+               /*Ignore first row and loop the csv row records and find each row record in nextRecord array.
+                nextRecords contain array of column value in single row.*/
+			csvReader.readNext();
+			while ((nextRecord = csvReader.readNext()) != null) {
+				cityList.add(nextRecord[7]);
+			}
+			//Convert TreeSet to List so we can search backward and forward for performance.
+			CitySuggestionApplication.cityList.addAll(cityList);
+			logger.info("City cached");
+
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage());
+		}
 	}
 
 }
--- src/main/java/com/pramati/suggestion/services/CitySuggestionImpl.java
+++ src/main/java/com/pramati/suggestion/services/CitySuggestionImpl.java
@@ -24,10 +24,6 @@ public class CitySuggestionImpl implements CitySuggestion {
     public String getSuggestion(String prefix, int atmost) {
        StringBuilder stringBuilder=new StringBuilder();
 
-       //Cache the cities
-       if(CitySuggestionApplication.cityList.size()==0)
-           redRecordFromCSV();
-
        List<String> records=getSuggestedCity(prefix,atmost);
        //If no records found return message;
        if(records.isEmpty())
@@ -36,34 +32,6 @@ public class CitySuggestionImpl implements CitySuggestion {
            stringBuilder.append(record+System.lineSeparator());
        }
        return stringBuilder.toString();
-    }
-
-
-    /**Open CSV used to read csv file row wise and match prefix if found then add in separate set.
-     * csv file also contain duplicate city name and case insensitive name.It will return unique suggestion name for city.
-     * Used TreeSet to contain unique city name and ignore case insensitive .
-     */
-    public void redRecordFromCSV()
-    {
-            try {
-                logger.info("Reading city List.....");
-                ClassPathResource cpr = new ClassPathResource("static/city_data.csv");
-                CSVReader csvReader = new CSVReader(new InputStreamReader(cpr.getInputStream()));
-                String[] nextRecord;
-                Set<String> cityList=new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-               /*Ignore first row and loop the csv row records and find each row record in nextRecord array.
-                nextRecords contain array of column value in single row.*/
-                csvReader.readNext();
-                while ((nextRecord = csvReader.readNext()) != null) {
-                    cityList.add(nextRecord[7]);
-                }
-              //Convert TreeSet to List so we can search backward and forward for performance.
-                CitySuggestionApplication.cityList.addAll(cityList);
-
-            }
-            catch (Exception e) {
-                logger.error(e.getMessage());
-            }
     }
 
     /**
