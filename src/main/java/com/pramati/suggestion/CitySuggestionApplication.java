d41537bd6bc62627ff94a9619d7c05c6b3df5f2b
Rohit Kumar <rohitkumar@Rohits-MacBook-Pro.local>
2019-06-05 12:56:22 +0530

    performance improvment

--- src/main/java/com/pramati/suggestion/CitySuggestionApplication.java
+++ src/main/java/com/pramati/suggestion/CitySuggestionApplication.java
@@ -3,8 +3,14 @@ package com.pramati.suggestion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 @SpringBootApplication
 public class CitySuggestionApplication {
+	public static List<String> cityList=new ArrayList<>();
 	public static void main(String[] args) {
 		SpringApplication.run(CitySuggestionApplication.class, args);
 	}
--- src/main/java/com/pramati/suggestion/services/CitySuggestionImpl.java
+++ src/main/java/com/pramati/suggestion/services/CitySuggestionImpl.java
@@ -3,19 +3,32 @@ package com.pramati.suggestion.services;
 
 import com.opencsv.CSVReader;
 
+import static com.pramati.suggestion.CitySuggestionApplication.cityList;
+import  com.pramati.suggestion.CitySuggestionApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 @Service
 public class CitySuggestionImpl implements CitySuggestion {
+
+    Logger logger = LoggerFactory.getLogger(CitySuggestionImpl.class);
 
     @Override
     public String getSuggestion(String prefix, int atmost) {
        StringBuilder stringBuilder=new StringBuilder();
-       Set<String> records=redRecordFromCSV(prefix,atmost);
+
+       //Cache the cities
+       if(CitySuggestionApplication.cityList.size()==0)
+           redRecordFromCSV();
+
+       List<String> records=getSuggestedCity(prefix,atmost);
        //If no records found return message;
        if(records.isEmpty())
            return "No city found.";
@@ -29,36 +42,60 @@ public class CitySuggestionImpl implements CitySuggestion {
     /**Open CSV used to read csv file row wise and match prefix if found then add in separate set.
      * csv file also contain duplicate city name and case insensitive name.It will return unique suggestion name for city.
      * Used TreeSet to contain unique city name and ignore case insensitive .
+     */
+    public void redRecordFromCSV()
+    {
+            try {
+                logger.info("Reading city List.....");
+                ClassPathResource cpr = new ClassPathResource("static/city_data.csv");
+                CSVReader csvReader = new CSVReader(new InputStreamReader(cpr.getInputStream()));
+                String[] nextRecord;
+                Set<String> cityList=new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+               /*Ignore first row and loop the csv row records and find each row record in nextRecord array.
+                nextRecords contain array of column value in single row.*/
+                csvReader.readNext();
+                while ((nextRecord = csvReader.readNext()) != null) {
+                    cityList.add(nextRecord[7]);
+                }
+              //Convert TreeSet to List so we can search backward and forward for performance.
+                CitySuggestionApplication.cityList.addAll(cityList);
+
+            }
+            catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+    }
+
+    /**
+     * Search city from cache, searching will happen forward and backward.
      * @param prefix
      * @param atmost
      * @return
      */
-    public Set<String> redRecordFromCSV(String prefix,  int atmost)
+    private List<String> getSuggestedCity(String prefix,int atmost)
     {
-        try {
-
-            ClassPathResource cpr = new ClassPathResource("static/city_data.csv");
-            CSVReader csvReader = new CSVReader(new InputStreamReader(cpr.getInputStream()));
-            Set<String> suggestion=new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-            String[] nextRecord;
-           /*Ignore first row and loop the csv row records and find each row record in nextRecord array.
-           nextRecords contain array of column value in single row.*/
-            csvReader.readNext();
-            while ((nextRecord = csvReader.readNext()) != null) {
-
-                //In each row city is at 7 column, change nextRecord index in case csv column ordering change.
-                if(nextRecord[7].toUpperCase().startsWith(prefix.toUpperCase()))
-                    suggestion.add(nextRecord[7]);
-
-                // check if atmost records have been completed, break the loop and return that list;
-                if(suggestion.size()==atmost)
-                    return suggestion;
+        logger.info("Getting suggested city");
+        List<String> suggestionList=new ArrayList<>();
+        int start=0, end=cityList.size()-1;
+        //convert prefix to upperCase to deal with case insensitive.
+        prefix=prefix.toUpperCase();
+        while (start<end&&suggestionList.size()<atmost)
+        {
+            if(cityList.get(start).toUpperCase().startsWith(prefix))
+            {
+                suggestionList.add(cityList.get(start));
             }
-            return suggestion;
+
+            if(cityList.get(end).toUpperCase().startsWith(prefix))
+            {
+                suggestionList.add(cityList.get(end));
+            }
+           start++;
+           end--;
+
         }
-        catch (Exception e) {
-            return Collections.emptySet();
-        }
+       return  suggestionList;
+
     }
 
 
